8dcfad53dd0b2089a4d19ed0524abe86a65060c3
Pa-Wa <130307836+Pa-Wa@users.noreply.github.com>
2023-07-16 15:40:55 +0200

    Update pg3.py

--- Dash/pages/pg3.py
+++ Dash/pages/pg3.py
@@ -10,7 +10,7 @@ from pandas_market_calendars import get_calendar
 import plotly.graph_objects as go
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import math
-from Methods.HoltWinter_func import HoltWinter
+from methods.HoltWinter_func import HoltWinter
 
 pd.options.mode.chained_assignment = None
 
