deeff7666b884facb0c02525799c039015ac7814
Yann Chevalaz <106061184+Yanito11@users.noreply.github.com>
2023-07-17 13:38:06 +0200

    ARIMA Prognose

---
+++ Forecast/evaluation_forecast/forecast_methods/Arima.py
@@ -0,0 +1,14 @@
+from pmdarima.arima import auto_arima
+import pandas as pd
+
+def ARIMA(historical_data):
+    """
+    Führt mittel ARIMA-Methode eine Kursprognose aus
+    :param historical_data: historische Daten 
+    :return forecast_df (DF der Vorhersage)
+    """
+    
+    model = auto_arima(historical_data, seasonal = True, stepwise = True, m = 6, method = "bfgs", maxiter = 20, suppress_warnings = True) #Definiert das Modell
+    forecasts = model.predict(n_periods = 10) #Vorhersage
+    forecast_df = pd.DataFrame(forecasts, columns=["Predicted Price"]) #Erstellt DF der Vorhersage
+    return forecast_df
