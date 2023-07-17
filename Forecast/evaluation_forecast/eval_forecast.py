d8a6703e042272ba5cd0a50859ea8cc3634fed88
Pa-Wa <130307836+Pa-Wa@users.noreply.github.com>
2023-07-17 11:56:34 +0200

    Update und Kommentierung

--- Forecast/evaluation_forecast/eval_forecast.py
+++ Forecast/evaluation_forecast/eval_forecast.py
@@ -23,51 +23,52 @@ def MAE(y_true, y_pred):
     mae_scaled = round(mae/average, 4)
     return mae_scaled
 
-aktien = ["ALV.DE", "DPW.DE", "AMZ.DE", "MDO.DE", "NVD.DE", "^MDAXI"]
-end_zeitpunkte = ["2023-02-18", "2023-05-17", "2023-07-07", "2022-12-16"]
-end_zeitpunkte_ts = [datetime.strptime(end_zeitpunkte[0], "%Y-%m-%d").date(),
+aktien = ["ALV.DE", "DPW.DE", "AMZ.DE", "MDO.DE", "NVD.DE", "^MDAXI"] #Ausgewählte Aktien zum Vergleich
+end_zeitpunkte = ["2023-02-18", "2023-05-17", "2023-07-07", "2022-12-16"] #Ausgewählte Zeiträume zum Vergleich
+end_zeitpunkte_ts = [datetime.strptime(end_zeitpunkte[0], "%Y-%m-%d").date(), #Wandle Datum in Date-Format um
                       datetime.strptime(end_zeitpunkte[1], "%Y-%m-%d").date(),
                       datetime.strptime(end_zeitpunkte[2], "%Y-%m-%d").date(),
                       datetime.strptime(end_zeitpunkte[3], "%Y-%m-%d").date()]
 
 #Zeitraum der historischen Daten anpassen: HW (5 Jahre), ARIMA(2 Jahre), LSTM-Modelle (3 Jahre)
 time_horizont = 3
-start_zeitpunkte_ts = [end_zeitpunkte_ts[0]-relativedelta(years=time_horizont),
+start_zeitpunkte_ts = [end_zeitpunkte_ts[0]-relativedelta(years=time_horizont), #Bestimme Start für die historischen Daten
                         end_zeitpunkte_ts[1]-relativedelta(years=time_horizont),
                         end_zeitpunkte_ts[2]-relativedelta(years=time_horizont),
                         end_zeitpunkte_ts[3]-relativedelta(years=time_horizont)]
 
-counter = 0
-for aktie in aktien:
-    data = yf.download(aktie, period = "max")
-    data.drop(columns = ["Open", "High", "Low", "Volume", "Adj Close"], axis = 1, inplace = True)
-    for i in range(len(end_zeitpunkte)):
+counter = 0 #Zur Erstellung des DF
+for aktie in aktien: #Iteriere über alle Aktien
+    data = yf.download(aktie, period = "max") #Downloade historische Daten
+    data.drop(columns = ["Open", "High", "Low", "Volume", "Adj Close"], axis = 1, inplace = True) #Entferne nicht benötigte Spalten
+    for i in range(len(end_zeitpunkte)): #Iteriere über alle Zeiträume
         print(aktie,i)
-        filtered_df = data.loc[start_zeitpunkte_ts[i]: end_zeitpunkte_ts[i]]
-        hist_data = filtered_df[: len(filtered_df) - 10]
-        hist_data_for_function = hist_data.copy()
-        prog_data = filtered_df[len(filtered_df) - 10: ]
-        index_list = prog_data.index.tolist()
+        filtered_df = data.loc[start_zeitpunkte_ts[i]: end_zeitpunkte_ts[i]] #Filtere Datensatz bezüglich des Zeitraums
+        hist_data = filtered_df[: len(filtered_df) - 10] #Historische Daten für Prognose
+        hist_data_for_function = hist_data.copy() #Für den Funktionsaufruf, da hist_data bei LSTM Methoden verändert wird
+        prog_data = filtered_df[len(filtered_df) - 10: ] #Tatsächliche Kurse der voherzusagenden Tage
+        index_list = prog_data.index.tolist() #Speichere Daten in Liste
 
-        forecast_df = NaivePrediction(hist_data_for_function)
+        forecast_df = NaivePrediction(hist_data_for_function) #Funktionsaufruf der Methode; Möglichkeiten: Holt-Winters, ARIMA, LSTM, LSTM_OS, NaivePrediction
 
-        forecast_df.set_index(pd.Index(index_list), inplace = True)
-        result = pd.concat([prog_data, forecast_df], axis = 1)
+        forecast_df.set_index(pd.Index(index_list), inplace = True) #Verändere Index zu Datum
+        result = pd.concat([prog_data, forecast_df], axis = 1) #Füge DF zusammen (untereinander)
+        #Berechne Kennzahlen
         one_day = MAE([result["Close"][0]], [result["Predicted Price"][0]])
         three_day = MAE(result["Close"][:3], result["Predicted Price"][:3])
         five_day = MAE(result["Close"][:5], result["Predicted Price"][:5])
         ten_day = MAE(result["Close"], result["Predicted Price"])
-        if counter == 0:
+        if counter == 0: #Falls 1. Run: Erstelle DF
             df_full = pd.DataFrame({counter: [one_day,three_day,five_day,ten_day]})
         else:
             df_new = pd.DataFrame({counter: [one_day,three_day,five_day,ten_day]})
-            df_full = df_full.join(df_new)
-        counter = counter + 1
+            df_full = df_full.join(df_new) #Füge DF zusammen (nebeneinander)
+        counter = counter + 1 #Erhöhe Counter, damit kein weiterer DF neu angelegt wird
 
-mittelwerte = df_full.mean(axis = 1) * 100
-mittelwerte_df = pd.DataFrame(mittelwerte, columns = ["average MPAE"])
-mittelwerte_df.loc[4] = mittelwerte_df["average MPAE"].mean()
-neuer_index = ["1Tag", "3Tage", "5Tage", "10Tage", "Average"]
-mittelwerte_df.set_index(pd.Index(neuer_index), inplace = True)
-print(mittelwerte_df)
-#mittelwerte_df.to_csv("solution_Arima_20Jahr.csv",sep = ";")
+average = df_full.mean(axis = 1) * 100 #Berechne Mittelwerte der einzelnen Kennzahlen
+average_df = pd.DataFrame(average, columns = ["average MPAE"]) #Erstelle DF
+average_df.loc[4] = average_df["average MPAE"].mean() #Berechne Durchschnitt der Kennzahlen
+new_index = ["1Tag", "3Tage", "5Tage", "10Tage", "Average"] 
+average_df.set_index(pd.Index(new_index), inplace = True) #Ändere Index
+print(average_df)
+#average_df.to_csv("solution_Naive.csv",sep = ";") #Speichere Ergebnisse im DF ab
