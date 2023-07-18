c441049ab4bffe489121c0383dfc3c1edaeb41e4
Pa-Wa <130307836+Pa-Wa@users.noreply.github.com>
2023-07-18 11:51:11 +0200

    Update Arima.py

--- Forecast/evaluation_forecast/forecast_methods/Arima.py
+++ Forecast/evaluation_forecast/forecast_methods/Arima.py
@@ -13,7 +13,7 @@ def ARIMA(historical_data):
     :return forecast_df (DF der Vorhersage)
     """
     
-    model = auto_arima(historical_data, seasonal = True, stepwise = True, m = 6, method = "bfgs", maxiter = 20, suppress_warnings = True) #Definiert das Modell
+    model = auto_arima(historical_data["Close"].tolist(), seasonal = True, stepwise = True, m = 6, method = "bfgs", maxiter = 20, suppress_warnings = True) #Definiert das Modell
     forecasts = model.predict(n_periods = 10) #Vorhersage
     forecast_df = pd.DataFrame(forecasts, columns=["Predicted Price"]) #Erstellt DF der Vorhersage
     return forecast_df
