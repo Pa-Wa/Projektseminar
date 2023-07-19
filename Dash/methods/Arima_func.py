242a4ea024a48f5a09259711cfc3f9a898662734
Pa-Wa <130307836+Pa-Wa@users.noreply.github.com>
2023-07-19 23:50:46 +0200

    Update Arima_func.py

--- Dash/methods/Arima_func.py
+++ Dash/methods/Arima_func.py
@@ -10,7 +10,7 @@ def Arima(historical_data, pred_days):
     :return fitted_values (angepasste Trainingsdaten)
     """
     
-    model = ARIMA(historical_data["Close"].tolist(), order = (0, 1, 0)) #Definiert das Modell
+    model = ARIMA(historical_data, order = (0, 1, 0)) #Definiert das Modell
     model_fit = model.fit() #Trainiert das Modell
     forecasts = model_fit.forecast(steps = pred_days) #Vorhersage
     forecast_df = pd.DataFrame(forecasts.tolist(), columns = ["Close"]) #Erstellt DF der Vorhersage
